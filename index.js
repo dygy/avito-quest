9bda56bc9f03b6af6d21a732a8e2262e6c29ce10
Dygy <Yukimurachris@gmail.com>
2019-07-29 18:01:28 +0300

    like mechanism

--- index.js
+++ index.js
@@ -7,77 +7,39 @@ if (myStorage.getItem('favorIndex')===undefined||myStorage.getItem('favorIndex')
     favorIndex = myStorage.getItem('favorIndex')
 }
 console.log(myStorage);
-const flexbox = document.getElementsByClassName('flexbox')[0]
+const flexbox = document.getElementsByClassName('flexbox')[0];
 const changeColor = (id, color) =>{
     document.getElementById(id).style.color = color
 };
-const toDarkMode = () =>{
-
-};
-const Post = class {
-    name='';
-    date='';
-    price='';
-    photo='';
-    city='';
-    type=''
-};
-const posts = [];
-
-let createPost = (name, city, price, photo, type)=> {
-    const post = new Post();
-    post.name = name;
-    post.city = city;
-    post.price = price+' ₽';
-    post.photo = photo;
-    let date = new Date();
-    if (date.getMinutes().toString().length === 2) {
-        post.date = 'Сегодня ' + date.getHours() + ' : ' + date.getMinutes();
-    }
-    else {
-        post.date = 'Сегодня ' + date.getHours() + ' :  0' + date.getMinutes();
-    }
-    post.type=type;
-    posts.push(post);
-    date = null;
-};
-for (let x=0 ;x<8;x++){
-    createPost('Avtamabil','Moskva','700 000','./imgs/autonews2-tnnw.jpg','Auto')
-}
 let addToFavor=(param)=>{
     console.log(favorIndex);
     myStorage.setItem('favor'+favorIndex,JSON.stringify(posts[param.path[0].className[param.path[0].className.length-1]]));
-   // console.log(param.path[0].className[(param.path[0].className.length-1)]);
-   // console.log(myStorage.getItem('favor'+favorIndex));
+    // console.log(param.path[0].className[(param.path[0].className.length-1)]);
+    // console.log(myStorage.getItem('favor'+favorIndex));
     favorIndex++;
     myStorage.setItem('favorIndex',favorIndex)
 };
-let showFavor = () =>{
-    for (let x=0;x<favorIndex;x++){
-        console.log(myStorage.getItem('favor'+x))
-    }
-};
-for (let x=0;x<posts.length;x++){
+let publishPost=(post)=>{
     const node = document.createElement("div");                 // Create a <li> node
-    node.className ='item '+posts[x].type;
-    node.alt='';
-    const symbolNode =document.createElement('i');
-    symbolNode.className='fa fa-heart-o upOnPhoto index'+x;
-    symbolNode.onclick= addToFavor;
-    const imgNode =document.createElement('img');
-        imgNode.src=posts[x].photo;
-        imgNode.style.width='240px';
-        imgNode.style.height='150px';
-        imgNode.style.zIndex=0;
-    const textNode =document.createElement('p');
+    node.className = 'item ' + post.type;
+    node.alt = '';
+    const symbolNode = document.createElement('i');
+    symbolNode.className = 'fa fa-heart-o upOnPhoto index' + post.id;
+    symbolNode.onclick = addToFavor;
+    const imgNode = document.createElement('img');
+    imgNode.src = post.photo;
+    imgNode.style.width = '240px';
+    imgNode.style.height = '150px';
+    imgNode.style.zIndex = 0;
+    const textNode = document.createElement('p');
     const priceNode = document.createElement('strong');
-        priceNode.innerText=posts[x].price;
+    priceNode.innerText = post.price;
     const nameNode = document.createElement('a');
-        nameNode.innerText=posts[x].name;
+    nameNode.innerText = post.name;
     const cityNode = document.createElement('span');
-        cityNode.innerText=posts[x].city;
+    cityNode.innerText = post.city;
     const dateNode = document.createElement('date');
-        dateNode.innerText=posts[x].date;
+    dateNode.innerText = post.date;
 
     // Create a text node
 
@@ -96,5 +58,77 @@ for (let x=0;x<posts.length;x++){
     textNode.appendChild(dateNode);
 
     flexbox.appendChild(node);     // Append <li> to <ul> with id="myList"
+};
+let postNum =1;
+const toDarkMode = () =>{
+};
 
+const Post = class {
+    name='';
+    date='';
+    price='';
+    photo='';
+    city='';
+    type='';
+    id
+};
+const posts = [];
+
+let createPost = (name, city, price, photo, type, dates,id)=> {
+    const post = new Post();
+    post.id=id;
+    postNum++;
+    post.name = name;
+    post.date = dates;
+    post.city = city;
+    post.price = price+' ₽';
+    post.photo = photo;
+    let date = new Date();
+    if (post.date === '') {
+        if (date.getMinutes().toString().length === 2) {
+            post.date = 'Сегодня ' + date.getHours() + ' : ' + date.getMinutes();
+        } else {
+            post.date = 'Сегодня ' + date.getHours() + ' :  0' + date.getMinutes();
+        }
+    }
+    post.type=type;
+    date = null;
+    publishPost(post)
+};
+let createNewPost = (name, city, price, photo, type, dates)=> {
+    const post = new Post();
+    post.id=postNum;
+    postNum++;
+    post.name = name;
+    post.date = dates;
+    post.city = city;
+    post.price = price+' ₽';
+    post.photo = photo;
+    let date = new Date();
+    if (post.date === '') {
+        if (date.getMinutes().toString().length === 2) {
+            post.date = 'Сегодня ' + date.getHours() + ' : ' + date.getMinutes();
+        } else {
+            post.date = 'Сегодня ' + date.getHours() + ' :  0' + date.getMinutes();
+        }
+    }
+    post.type=type;
+    posts.push(post);
+    date = null;
+    publishPost(post)
+};
+for (let x=0 ;x<8;x++){
+    createNewPost('Avtamabil','Moskva','700 000','./imgs/autonews2-tnnw.jpg','Auto','')
 }
+
+let showFavor = () =>{
+    let lngth= document.getElementsByClassName('item').length;
+    for (let y=0;y<lngth;y++){
+        document.getElementsByClassName('item')[0].parentElement.removeChild(document.getElementsByClassName('item')[0])
+    }
+    for (let x=0;x<favorIndex;x++){
+        const item =JSON.parse(myStorage.getItem('favor'+x));
+        console.log(item);
+        createPost(item.name,item.circle,item.price,item.photo,item.type,item.date)
+    }
+};
