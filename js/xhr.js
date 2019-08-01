eaad4b8f2c79fba3284792a7f6f2387322ddacd6
Dygy <Yukimurachris@gmail.com>
2019-08-01 11:12:50 +0300

    upgraded search and dark mode

--- js/xhr.js
+++ js/xhr.js
@@ -1,27 +1,16 @@
-function loadXMLDoc() {
-    let xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState === XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
-            if (xmlhttp.status === 200) {
-                const obj =JSON.parse(xmlhttp.responseText);
-            //    console.log(obj.data);
-                for (let x=0;x<15;x++) {
-                    toPost(obj.data[x])
-                }
-
+function fetching() {
+    fetch("http://avito.dump.academy/products/")
+        .then(response=>{
+            return response.json()
+        })
+        .then(function (defs) {
+            for (let x=0;x<10 ;x++) {
+                const post = defs.data[x];
+                toPost(post)
             }
-            else if (xmlhttp.status === 400) {
-                alert('There was an error 400');
-            }
-            else {
-                alert('something else other than 200 was returned');
-            }
-        }
-    };
-
-    xmlhttp.open("GET", "http://avito.dump.academy/products/", true);
-    xmlhttp.send();
+        })
+        .catch(alert);
 }
 function toPost(data) {
     console.log(data);
@@ -29,8 +18,8 @@ function toPost(data) {
         createNewPost(data.title, data.address, data.price.toString(), data.pictures[0], data.category, data.year)
     }
 }
-loadXMLDoc();
 
+fetching();
 
 
 /*
@@ -73,7 +62,7 @@ geocoder.geocode(
 window.onscroll = function(ev) {
     if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
         // you're at the bottom of the page
-        loadXMLDoc();
+        fetching();
         console.log("Bottom of page");
     }
 };
